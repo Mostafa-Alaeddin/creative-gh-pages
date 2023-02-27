b9dc0f52efb6164d0522a3c4d562c6ac20457589
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-27 19:24:26 +0330

    added require once function.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,1 +1,13 @@
 <?php
+
+    require_once realpath(
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'
+    );
+
+    require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'function.php');
+
+
+
+
+
+    die();
