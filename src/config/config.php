c3a647789106885a6496a37aa1edbc5ed4fe89ba
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-27 19:33:05 +0330

    added comment's

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,10 @@
 <?php
 
+//    added autoload file
     require_once realpath(
         __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'
     );
 
-    require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'function.php');
+//    added database file
+    require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'database.php');
 
-
-
-
-
-    die();
