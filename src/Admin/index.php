4913189036d9edcb0e63d3bfcf2bb2d64b669ecf
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-28 17:11:16 +0330

    added require once head and footer tag

--- src/Admin/index.php
+++ src/Admin/index.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html dir="rtl" lang="en">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
-    <title>Nice admin Template - The Ultimate Multipurpose admin template</title>
-    <!-- Custom CSS -->
-    <link href="assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
-    <link href="assets/extra-libs/c3/c3.min.css" rel="stylesheet">
-    <link href="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
-    <!-- Custom CSS -->
-    <link href="dist/css/style.min.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-
-<body>
+    require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'partial' . DIRECTORY_SEPARATOR . 'head.inc.php') ?>
 
     <div class="preloader">
         <div class="lds-ripple">
@@ -36,10 +11,14 @@
 
     <div id="main-wrapper">
 
-       <?php require_once realpath(__DIR__.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'header.inc.php')?>
+        <?php
+            require_once realpath(
+                __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'header.inc.php'
+            ) ?>
 
 
-        <?php require_once realpath(__DIR__.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'aside.inc.php')?>
+        <?php
+            require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'aside.inc.php') ?>
 
 
         <div class="page-wrapper">
@@ -76,25 +55,5 @@
 
     </div>
 
-
-    <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-    <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="dist/js/app.min.js"></script>
-    <script src="dist/js/app.init.js"></script>
-    <script src="dist/js/app-style-switcher.js"></script>
-    <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-    <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-    <script src="dist/js/waves.js"></script>
-    <script src="dist/js/sidebarmenu.js"></script>
-    <script src="dist/js/custom.js"></script>
-<!--    <script src="assets/libs/chartist/dist/chartist.min.js"></script>-->
-<!--    <script src="assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>-->
-<!--    <script src="assets/extra-libs/c3/d3.min.js"></script>-->
-<!--    <script src="assets/extra-libs/c3/c3.min.js"></script>-->
-<!--    <script src="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>-->
-<!--    <script src="assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>-->
-<!--    <script src="dist/js/pages/dashboards/dashboard1.js"></script>-->
-</body>
-
-</html>
+<?php
+    require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'partial' . DIRECTORY_SEPARATOR . 'footer.inc.php');
