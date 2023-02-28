7830fc167f2b27bd192219c7448abf49a78dc42a
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-28 10:01:33 +0330

    debgu : append css code to style tag

--- src/login.php
+++ src/login.php
@@ -68,6 +68,40 @@
             white-space: nowrap;
             -webkit-overflow-scrolling: touch;
         }
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+        }
+
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+        }
+
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
     </style>
 
 
@@ -78,10 +112,7 @@
 
 <main class="form-signin w-100 m-auto">
 <!--    start login form-->
-    <div class="container">
-        <div class="row d-flex justify-content-center my-5">
-            <div class="col-3">
-                <form>
+    <form>
                     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
                     <div class="form-floating my-2">
@@ -94,10 +125,7 @@
                     </div>
                     <button class="w-100 btn btn-lg btn-primary my-2" type="submit">Sign in</button>
                     <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-                </form>
-            </div>
-        </div>
-    </div>
+        </form>
 <!--    end login form-->
 </main>
 
