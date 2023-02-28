e30ebd7e8204660f4d285035b79c18ea1d3b44f5
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-28 17:12:30 +0330

    added head and footer tag

---
+++ src/Admin/partial/footer.inc.php
@@ -0,0 +1,22 @@
+
+<script src="assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+<script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="dist/js/app.min.js"></script>
+<script src="dist/js/app.init.js"></script>
+<script src="dist/js/app-style-switcher.js"></script>
+<script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+<script src="assets/extra-libs/sparkline/sparkline.js"></script>
+<script src="dist/js/waves.js"></script>
+<script src="dist/js/sidebarmenu.js"></script>
+<script src="dist/js/custom.js"></script>
+<!--    <script src="assets/libs/chartist/dist/chartist.min.js"></script>-->
+<!--    <script src="assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>-->
+<!--    <script src="assets/extra-libs/c3/d3.min.js"></script>-->
+<!--    <script src="assets/extra-libs/c3/c3.min.js"></script>-->
+<!--    <script src="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>-->
+<!--    <script src="assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>-->
+<!--    <script src="dist/js/pages/dashboards/dashboard1.js"></script>-->
+</body>
+
+</html>
