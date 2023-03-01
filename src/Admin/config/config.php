a3ea25df8f4f854d1b915a95a60e11bdfd422094
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-03-01 09:29:26 +0330

    test: test to work or not !

--- src/Admin/config/config.php
+++ src/Admin/config/config.php
@@ -1,1 +1,12 @@
 <?php
+
+    require_once realpath(
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'
+    );
+
+    $dotEnv = \Dotenv\Dotenv::createImmutable(
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+    );
+    echo "<pre>";
+    var_dump($dotEnv);
+    die();
